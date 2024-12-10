6268a1fc788b772f4a2ec04d129e514a6cb08c83
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-10 18:32:02 +0530

    refactor: Wrap app by shopContextProvider

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,16 +1,15 @@
-import { createContext } from 'react';
-import { products } from '../assets/frontend_assets/assets';
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
+
     const currency = '$';
     const delivery_fee = 10;
 
     const value = {
-        products,
-        currency,
-        delivery_fee
+        products, currency, delivery_fee
     }
 
     return (
