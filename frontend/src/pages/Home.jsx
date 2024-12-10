84a4ca4ff914330e982df503022188bda3e3396f
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-10 19:40:46 +0530

    feat: implement bestseller component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestColection from '../components/LatestColection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestColection/>
+      <BestSeller/>
     </div>
   )
 }
