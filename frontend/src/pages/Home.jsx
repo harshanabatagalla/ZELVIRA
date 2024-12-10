0b2874d4aa6692d0062f92883b77a51b597d1f29
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-10 18:33:32 +0530

    feat: implement latest collection component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import LatestColection from '../components/LatestColection'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LatestColection/>
     </div>
   )
 }
