9f16095a92c077b18a316767df9d58be6e85664c
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-10 19:56:55 +0530

    feat: implement our policy section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestColection from '../components/LatestColection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPOlicy'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <LatestColection/>
       <BestSeller/>
+      <OurPolicy/>
     </div>
   )
 }
