2c08d07cff5ebeb2a2a6f0991a3723470f790972
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-10 17:02:28 +0530

    refactor: add custom fonts

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -11,14 +11,14 @@ const Hero = () => {
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                         <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
                     </div>
-                    <h1 className='text-3xl sm:py-3 lg:text leading-relaxed'>Latest Arrivals</h1>
+                    <h1 className='text-3xl sm:py-3 lg:text leading-relaxed prata-regular'>Latest Arrivals</h1>
                     <div className='flex items-center gap-2'>
                         <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                     </div>
                 </div>
             </div>
-            {/* hHero right side */}
+            {/* Hero right side */}
             <img className='w-full sm:w-1/2 object-cover' src={assets.hero_img} alt='hero' />
         </div>
     )
