e4c4fbab8ead68c018648917cd9d175d7e09a93e
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-16 18:09:41 +0530

    feat: setup backend and configure mongodb

---
+++ backend/config/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("MongoDB connected");
+        });
+        
+        await mongoose.connect(`${process.env.MONGODB_URI}/zelvira`);
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello World'));
+
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
