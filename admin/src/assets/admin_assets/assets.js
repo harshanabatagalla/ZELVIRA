81fcf28af39e2285f738233858e1ef3fb9de573d
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-21 22:40:21 +0530

    refactor: admin/change list icon

--- admin/src/assets/admin_assets/assets.js
+++ admin/src/assets/admin_assets/assets.js
@@ -1,6 +1,7 @@
 import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
+import list_icon from './list_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
@@ -8,6 +9,7 @@ export const assets = {
     logo,
     add_icon,
     order_icon,
+    list_icon,
     upload_area,
     parcel_icon
 }
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
                 </NavLink>
 
                 <NavLink to='/list' className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l hover:bg-gray-100'>
-                    <img className='w-5 h-5' src={assets.parcel_icon} alt="list" />
+                    <img className='w-5 h-5' src={assets.list_icon} alt="list" />
                     <p className='hidden md:block'>List Items</p>
                 </NavLink>
 
@@ -21,7 +21,7 @@ const Sidebar = () => {
                     <img className='w-5 h-5' src={assets.order_icon} alt="orders" />
                     <p className='hidden md:block'>Orders</p>
                 </NavLink>
-                
+
             </div>
 
         </div>
