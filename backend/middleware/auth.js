61fbe1e3f569c5cb52b55a833a39b2cc55bf381e
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-12-24 18:49:47 +0530

    feat: implement user authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+    try {
+        const { token } = req.headers;
+        if (!token) {
+            return res.json({ success: false, message: "Access denied" });
+        }
+        else {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.body.userId = decoded.id;
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export default authUser;
